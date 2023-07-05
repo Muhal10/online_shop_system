c5928466a1498a8e36c83d1108493a9ebed083fa
Muhal10 <129385993+Muhal10@users.noreply.github.com>
2023-07-05 12:06:07 +0300

    drag commit

---
+++ admin/customers.php
@@ -0,0 +1,154 @@
+<?php session_start(); ?>
+<?php include_once("./templates/top.php"); ?>
+<?php include_once("./templates/navbar.php"); ?>
+<div class="container-fluid">
+  <div class="row">
+
+    <?php include "./templates/sidebar.php"; ?>
+
+    <?php
+include("../db.php");
+if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='delete')
+{
+$user_id=$_GET['user_id'];
+
+/*this is delet quer*/
+mysqli_query($con,"delete from user_info where user_id='$user_id'")or die("query is incorrect...");
+}
+
+?>
+      <!-- End Navbar -->
+      <div class="content">
+        <div class="container-fluid">
+         <div class="col-md-14">
+            <div class="card ">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title">Manage User</h4>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive ps">
+                  <table class="table tablesorter table-hover" id="">
+                    <thead class=" text-primary">
+                      <tr><th>User id</th>
+                        <th>Name</th>
+                        <th>last_name</th>
+                        <th>Email</th>
+
+                <th>User Password</th>
+                <th>mobile</th>
+                <th>city</th>
+                <th>Address</th>
+	<th><a href="addcustomer.php" class="btn btn-success">Add New</a></th>
+                    </tr></thead>
+                    <tbody>
+                      <?php
+                        $result=mysqli_query($con,"select user_id,first_name,last_name, email, password,mobile,address1,address2 from user_info")or die ("query 2 incorrect....");
+
+                        while(list($user_id,$user_name,$user_last,$email,$user_password,$mobile,$address1,$address2)=
+                        mysqli_fetch_array($result))
+                        {
+                        echo "<tr>
+                        <td>$user_id</td>
+                         <td>$user_name</td>
+                          <td>$user_last</td>
+                           <td>$email</td>
+
+                        <td>$user_password</td>
+                         <td>$mobile</td>
+                          <td>$address1</td>
+                           <td>$address2</td>";
+                        echo"<td>
+
+                        <a class='btn btn-danger' href='customers.php?user_id=$user_id&action=delete'>Delete<div class='ripple-container'></div></a>
+                        </td></tr>";
+                        }
+                        mysqli_close($con);
+                        ?>
+                    </tbody>
+                  </table>
+                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="add_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form id="add-product-form" enctype="multipart/form-data">
+        	<div class="row">
+        		<div class="col-12">
+        			<div class="form-group">
+		        		<label>Product Name</label>
+		        		<input type="text" name="product_name" class="form-control" placeholder="Enter Product Name">
+		        	</div>
+        		</div>
+        		<div class="col-12">
+        			<div class="form-group">
+		        		<label>Brand Name</label>
+		        		<select class="form-control brand_list" name="brand_id">
+		        			<option value="">Select Brand</option>
+		        		</select>
+		        	</div>
+        		</div>
+        		<div class="col-12">
+        			<div class="form-group">
+		        		<label>Category Name</label>
+		        		<select class="form-control category_list" name="category_id">
+		        			<option value="">Select Category</option>
+		        		</select>
+		        	</div>
+        		</div>
+        		<div class="col-12">
+        			<div class="form-group">
+		        		<label>Product Description</label>
+		        		<textarea class="form-control" name="product_desc" placeholder="Enter product desc"></textarea>
+		        	</div>
+        		</div>
+        		<div class="col-12">
+        			<div class="form-group">
+		        		<label>Product Price</label>
+		        		<input type="number" name="product_price" class="form-control" placeholder="Enter Product Price">
+		        	</div>
+        		</div>
+        		<div class="col-12">
+        			<div class="form-group">
+		        		<label>Product Keywords <small>(eg: apple, iphone, mobile)</small></label>
+		        		<input type="text" name="product_keywords" class="form-control" placeholder="Enter Product Keywords">
+		        	</div>
+        		</div>
+        		<div class="col-12">
+        			<div class="form-group">
+		        		<label>Product Image <small>(format: jpg, jpeg, png)</small></label>
+		        		<input type="file" name="product_image" class="form-control">
+		        	</div>
+        		</div>
+        		<input type="hidden" name="add_product" value="1">
+        		<div class="col-12">
+        			<button type="button" class="btn btn-primary add-product">Add Product</button>
+        		</div>
+        	</div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal -->
+
+<?php include_once("./templates/footer.php"); ?>
+
+
+
+<script type="text/javascript" src="./js/customers.js"></script>
---
+++ admin/js/dashboard.js
@@ -0,0 +1,54 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  window.onload = function() {
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+  var chart = new chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })};
+}())
